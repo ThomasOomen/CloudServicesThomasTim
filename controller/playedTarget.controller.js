60e4a14c716e5bd183746f060eaf2d575c6f1d46
Thomas <twh.oomen@student.avans.nl>
2022-04-26 20:44:30 +0200

    'j'm

--- controller/playedTarget.controller.js
+++ controller/playedTarget.controller.js
@@ -153,8 +153,9 @@ exports.newPlayedTarget = (req, res) => {
                                                         let compareScorePlayedTarget = [];
                                                         for (let i = 0; i < playedTargetScore.length; i++) {
                                                             console.log('playedTargetScore[i][confidence]: ', playedTargetScore[i]['confidence']);
-                                                            if (playedTargetScore[i]['confidence'] > minimalConfidence)
+                                                            if (playedTargetScore[i]['confidence'] > minimalConfidence) {
                                                                 compareScorePlayedTarget[playedTargetScore[i]['tag']['en']] = playedTargetScore[i]['confidence'];
+                                                            }
                                                         }
 
                                                         const allowedScoreOffset = 15;
