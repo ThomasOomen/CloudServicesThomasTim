7fdaf1a60ce079b00c9e5c9b3e6e634d66d62930
Thomas <twh.oomen@student.avans.nl>
2022-04-27 21:27:33 +0200

    m

--- controller/playedTargetScore.controller.js
+++ controller/playedTargetScore.controller.js
@@ -11,7 +11,7 @@ exports.getPlayedTargetScore = (req, res) => {
             });
         }
         else {
-            console.log('req.params.id:', req.params.id);
+            console.log('req.params.id:', req.params);
             console.log("lel: ", Object.keys(playedTarget['target']).includes(req.params.id));
             if (Object.keys(playedTarget['target']).includes(req.params.id)) {
                 console.log("in hier");
